41738ea21d65e915c791772cb632af847989bbb9
Aljoboyer <tanveeazmain@gmail.com>
2024-08-24 16:21:46 +0600

    update api done

--- src/app/controller/Blog.controller.js
+++ src/app/controller/Blog.controller.js
@@ -28,20 +28,65 @@ const blog_create_controller = async (req, res) => {
 
 
 const getBlogsController = async (req, res) => {
-    const products = await BlogCollection.find({}).populate('writtenBy');
-    res.send(products);
+    const blogs = await BlogCollection.find({}).populate('writtenBy');
+    res.send(blogs);
   };
 
   const getSingleBlogController = async (req, res) => {
-    console.log('params ', req.params.id)
     const objectIdQuery = new ObjectId(req.params.id);
 
     const blog = await BlogCollection.findOne({_id: objectIdQuery }).populate('writtenBy');
     res.send(blog);
   };
 
+  const getPersonalBlogsController = async (req, res) => {
+
+    const objectIdQuery = new ObjectId(req.params.id);
+
+    const blogs = await BlogCollection.find({writtenBy: objectIdQuery }).populate('writtenBy');
+    res.send(blogs);
+  };
+
+  
+const updateBlogController = async (req, res) => {
+
+  try {
+      const {title, description}  = req.body;
+      const query = {_id: new ObjectId(req.body.editId)};
+
+      if(req?.files){
+          const imgdata = req.files.blogImg.data;
+          const encodedpic = imgdata.toString('base64');
+          const blogImg = Buffer.from(encodedpic, 'base64');
+
+          const updateBlog = await BlogCollection.findOneAndUpdate(query, {
+            $set: {
+            title: title,
+            description: description,
+            blogImg: blogImg
+             },
+          });
+      }
+      else{
+        const updateBlog = await BlogCollection.findOneAndUpdate(query, {
+          $set: {
+          title: title,
+          description: description,
+           },
+        });
+      }
+
+      res.json({msg: 'update success'})
+  } catch (error) {
+      res.status(400).json({error});
+      
+  }
+};
+
 module.exports = {
     blog_create_controller,
     getBlogsController,
-    getSingleBlogController
+    getSingleBlogController,
+    getPersonalBlogsController,
+    updateBlogController
   };
--- src/app/routes/blog.route.js
+++ src/app/routes/blog.route.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 
-const { blog_create_controller, getBlogsController, getSingleBlogController } = require("../controller/Blog.controller");
+const { blog_create_controller, getBlogsController, getSingleBlogController, getPersonalBlogsController, updateBlogController } = require("../controller/Blog.controller");
 const JwtTokenChecker = require("../middlewares/JwtTokenChecker");
 
 router.post("/publish",JwtTokenChecker, blog_create_controller);
 router.get("/allblogs", getBlogsController);
 router.get("/singleblog/:id", getSingleBlogController);
+router.get("/personalblogs/:id", getPersonalBlogsController);
+router.put("/update",JwtTokenChecker, updateBlogController);
 
 module.exports = router;
